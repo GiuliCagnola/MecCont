8f9e091a501109eaff241245dcd83324abf42100
Giuliana Cagnola <98410646+GiuliCagnola@users.noreply.github.com>
2025-04-10 11:18:29 -0300

    Update fuerza.m

--- fuerza.m
+++ fuerza.m
@@ -7,6 +7,13 @@ function F = fuerza(xi_0, xj_0, xi, xj, k)
 
   L0 = sqrt((xj_0(1) - xi_0(1))^2 + (xj_0(2) - xi_0(2))^2);
   L = sqrt((xj(1) - xi(1))^2 + (xj(2) - xi(2))^2);
+  
+  #-----Grandes deformaciones
   Fesc = k*(1-L0/L);
   F(1) = Fesc*(xj(1) - xi(1)); #Fx
   F(2) = Fesc*(xj(2) - xi(2)); #Fy
+
+  #-----Pequeñas deformaciones
+  #Fesc = k*(L/L0-1);
+  #F(1) = Fesc*(xj_0(1) - xi_0(1)); #Fx
+  #F(2) = Fesc*(xj_0(2) - xi_0(2)); #Fy
